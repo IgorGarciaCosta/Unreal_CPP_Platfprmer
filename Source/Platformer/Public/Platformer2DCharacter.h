e376a8043e723770bddc83a1012ae2b368b42aeb
Igor Garcia <igonildo7@gmail.com>
2024-11-23 14:57:42 -0300

    Created base classes for paper character and player controller

---
+++ Source/Platformer/Public/Platformer2DCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperCharacter.h"
+#include "Platformer2DCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PLATFORMER_API APlatformer2DCharacter : public APaperCharacter
+{
+	GENERATED_BODY()
+	
+};
