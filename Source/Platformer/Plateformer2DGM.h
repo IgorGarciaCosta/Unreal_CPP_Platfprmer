9b75bf6618a4ef3859edfef7497478c30f8e33b6
Igor Garcia <igonildo7@gmail.com>
2024-11-23 14:47:48 -0300

    Created game mode in cpp

---
+++ Source/Platformer/Plateformer2DGM.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "Plateformer2DGM.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PLATFORMER_API APlateformer2DGM : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
