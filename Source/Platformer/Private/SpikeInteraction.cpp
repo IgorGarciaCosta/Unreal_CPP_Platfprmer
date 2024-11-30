ea1f73e7e9e40f70586c11457ca9be3756f97958
Igor Garcia <igonildo7@gmail.com>
2024-11-30 00:50:50 -0300

    Spikes cpp class

---
+++ Source/Platformer/Private/SpikeInteraction.cpp
@@ -0,0 +1,57 @@
+
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SpikeInteraction.h"
+#include "PaperSpriteComponent.h"
+#include "Components/BoxComponent.h"
+
+ASpikeInteraction::ASpikeInteraction()
+{
+	BoxComp = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxComp"));
+	BoxComp->SetupAttachment(PaperSprite);
+}
+
+void ASpikeInteraction::ChangeSpikeStats()
+{
+	bSpikeUp = !bSpikeUp;
+
+	if (bSpikeUp) {
+		BringSpikeDown();
+	}
+	else {
+		BringSpikeUp();
+	}
+}
+
+void ASpikeInteraction::BringSpikeUp()
+{
+	if (SpriteSpikeUp) {
+		PaperSprite->SetSprite(SpriteSpikeUp);
+		BoxComp->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	}
+}
+
+void ASpikeInteraction::BringSpikeDown()
+{
+	if (SpriteSpikeDown) {
+		PaperSprite->SetSprite(SpriteSpikeDown);
+		BoxComp->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	}
+}
+
+void ASpikeInteraction::BeginPlay()
+{
+	Super::BeginPlay();
+	BoxComp->OnComponentBeginOverlap.AddDynamic(this, &ASpikeInteraction::BoxBeginOverlap);
+
+}
+
+void ASpikeInteraction::BoxBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (PlayerChar == nullptr) return;
+
+	if (OtherActor == PlayerChar) {
+		PlayerChar->TakeDamage(DamageAmount, FDamageEvent(), nullptr, nullptr);
+	}
+}
---
+++ Source/Platformer/Public/SpikeInteraction.h
@@ -0,0 +1,60 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ParentActorInteraction.h"
+#include "Platformer2DCharacter.h"
+#include "SpikeInteraction.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PLATFORMER_API ASpikeInteraction : public AParentActorInteraction
+{
+	GENERATED_BODY()
+
+public:
+
+	ASpikeInteraction();
+
+	void ChangeSpikeStats();
+
+	UFUNCTION(BlueprintCallable)
+	void BringSpikeUp();
+	UFUNCTION(BlueprintCallable)
+	void BringSpikeDown();
+
+
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere)
+	bool bSpikeUp = false;
+
+	UPROPERTY(EditAnywhere)
+	bool bLooping = true;
+
+	UPROPERTY(EditAnywhere)
+	float DamageAmount = 10;
+
+	UPROPERTY(EditAnywhere)
+	float Duration = 4;
+
+	UPROPERTY(VisibleAnywhere)
+	class UBoxComponent* BoxComp;
+
+	UFUNCTION(BlueprintCallable)
+	void BoxBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UPROPERTY(EditAnywhere)
+	class UPaperSprite* SpriteSpikeUp;
+
+	UPROPERTY(EditAnywhere)
+	UPaperSprite* SpriteSpikeDown;
+
+	FTimerHandle TimerHandle;
+	
+};
