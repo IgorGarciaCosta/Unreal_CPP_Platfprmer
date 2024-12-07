1fb343fcc5424b06ec2559676914303399eb87f9
Igor Garcia <igonildo7@gmail.com>
2024-12-07 17:58:15 -0300

    Create finishing game flag flipbook an dcreated a finishing game interaction component

---
+++ Source/Platformer/Public/FinishingGameInteraction.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ParentActorInteraction.h"
+#include "FinishingGameInteraction.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PLATFORMER_API AFinishingGameInteraction : public AParentActorInteraction
+{
+	GENERATED_BODY()
+	
+};
