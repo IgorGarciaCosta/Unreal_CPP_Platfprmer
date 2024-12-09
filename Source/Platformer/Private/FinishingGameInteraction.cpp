e177a0d75b3d59cac6648e5b5f155bcd462d71a9
Igor Garcia <igonildo7@gmail.com>
2024-12-09 13:29:31 -0300

    Added finishing level component with animated flag and sound

--- Source/Platformer/Private/FinishingGameInteraction.cpp
+++ Source/Platformer/Private/FinishingGameInteraction.cpp
@@ -2,4 +2,49 @@
 
 
 #include "FinishingGameInteraction.h"
+#include <CharacterGameComponent.h>
+#include <Kismet/GameplayStatics.h>
 
+AFinishingGameInteraction::AFinishingGameInteraction()
+{
+	BoxComp = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxComp"));
+	BoxComp->SetupAttachment(RootComponent);
+
+	EnterAreaSound = CreateDefaultSubobject<UAudioComponent>(TEXT("EnterAreaSound"));
+	EnterAreaSound->SetupAttachment(RootComponent);
+	EnterAreaSound->bAutoActivate = false; // Do not play sound on start
+
+	PaperFlipbookComponent = CreateDefaultSubobject<UPaperFlipbookComponent>(TEXT("PaperFlipbookComponent"));
+	PaperFlipbookComponent->SetupAttachment(RootComponent);
+}
+
+void AFinishingGameInteraction::BeginPlay()
+{
+	Super::BeginPlay();
+	BoxComp->OnComponentBeginOverlap.AddDynamic(this, &AFinishingGameInteraction::BoxBeginOverlap);
+	BoxComp->OnComponentEndOverlap.AddDynamic(this, &AFinishingGameInteraction::BoxEndOverlap);
+
+	PaperFlipbookComponent->Stop();
+}
+
+void AFinishingGameInteraction::BoxBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (PlayerChar == nullptr) return;
+
+	if (OtherActor == PlayerChar) {
+		PaperFlipbookComponent->Play();
+		MoveFlagUp();
+
+		// Play the sound
+		if (EnterAreaSound && EnterAreaSoundBase)
+		{
+			UGameplayStatics::PlaySound2D(GetWorld(), EnterAreaSoundBase, 1, 1, 0);
+		}
+
+		
+	}
+}
+
+void AFinishingGameInteraction::BoxEndOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+}
--- Source/Platformer/Public/FinishingGameInteraction.h
+++ Source/Platformer/Public/FinishingGameInteraction.h
@@ -4,6 +4,9 @@
 
 #include "CoreMinimal.h"
 #include "ParentActorInteraction.h"
+#include "Components/BoxComponent.h"
+#include "Components/AudioComponent.h"
+#include "PaperFlipbookComponent.h"
 #include "FinishingGameInteraction.generated.h"
 
 /**
@@ -13,5 +16,36 @@ UCLASS()
 class PLATFORMER_API AFinishingGameInteraction : public AParentActorInteraction
 {
 	GENERATED_BODY()
-	
+
+public:
+	AFinishingGameInteraction();
+protected:
+	virtual void BeginPlay();
+
+
+	UPROPERTY(VisibleAnywhere)
+	class UBoxComponent* BoxComp;
+
+	// Sound component to play sound when gem is collected
+	UPROPERTY(VisibleAnywhere)
+	class UAudioComponent* EnterAreaSound;
+
+	// Sound to be played
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Sound")
+	class USoundBase* EnterAreaSoundBase;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	class UPaperFlipbookComponent* PaperFlipbookComponent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	class UPaperFlipbook* WavingFlag;
+
+	UFUNCTION(BlueprintCallable)
+	void BoxBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	UFUNCTION(BlueprintCallable)
+	void BoxEndOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent)
+	void MoveFlagUp();
+
 };
